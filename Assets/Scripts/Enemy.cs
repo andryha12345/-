49c2cfb954714d1693fe12656a56bade37fc566e
andryha12345 <112823290+andryha12345@users.noreply.github.com>
2024-04-20 15:00:00 +0300

    Update

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public UnityEngine.AI.NavMeshAgent enemy;
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemy.SetDestination(player.position);
+    }
+}
